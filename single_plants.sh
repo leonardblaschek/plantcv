416eb27ed78ba80aaf84726e46403513a82633a1
leonardblaschek <l.blaschek@vivaldi.net>
2019-04-02 12:15:28 +0200

    update tray-vis

---
+++ single_plants.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Here we are running a VIS top-view pipeline over a flat directory of images
+
+# Image names for this example look like this: cam1-16-08-06_16:45_el1100s1_p19.jpg
+
+/home/leonard/Applications/plantcv/plantcv-pipeline.py \
+-d /home/leonard/Documents/Uni/PhD/Phenotyping/2019-03-26_Laccase_mutants/Trays/test \
+-a filename \
+-p /home/leonard/Applications/plantcv/tray_vis.py \
+-s test.sqlite3 \
+-i /home/leonard/Documents/Uni/PhD/Phenotyping/2019-03-26_Laccase_mutants/Trays/single_plants/output \
+-f id \
+-t jpg \
+-T 8 \
+-w
