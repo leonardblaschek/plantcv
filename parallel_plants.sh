1fd760ead300b56bc23e8aa8fb593fdcc4906f96
leonardblaschek <l.blaschek@vivaldi.net>
2021-07-07 19:33:36 +0200

    update bash scripts

--- parallel_plants.sh
+++ parallel_plants.sh
@@ -2,14 +2,14 @@
 
 # for an explanation of the arguments, see https://plantcv.readthedocs.io/en/stable/pipeline_parallel/
 
-/home/leonard/Applications/plantcv/plantcv-pipeline.py \
--d /home/leonard/Dropbox/2020-01_LAC_phenotyping/images/front/renamed/harvesting/ \
+/home/leonard/Applications/miniconda3/envs/plantcv/bin/plantcv-workflow.py \
+-d /home/leonard/Documents/Uni/PhD/Phenotyping/2020-09_lac_mutants/front/renamed/ \
 -a filename \
--p /home/leonard/Applications/plantcv/plantcv_vis.py \
+-p /home/leonard/Applications/plantCV/plantcv_vis.py \
 -s %Y%m%d \
 -j results.json \
--i /home/leonard/Dropbox/2020-01_LAC_phenotyping/images/front/output/ \
--f timestamp,id,frame \
+-i /home/leonard/Dropbox/2021-05_plantcv_processing/2020-09_front/ \
+-f timestamp,id \
 -t jpg \
--T 4 \
+-T 6 \
 -w
--- parallel_trays.sh
+++ parallel_trays.sh
@@ -2,7 +2,7 @@
 
 # for an explanation of the arguments, see https://plantcv.readthedocs.io/en/stable/pipeline_parallel/
 
-/home/leonard/Applications/plantcv/plantcv-pipeline.py \
+/home/leonard/Applications/miniconda3/envs/plantcv/bin/plantcv-workflow.py \
 -d /home/leonard/Documents/Uni/PhD/Phenotyping/2020-05_lac11_segregating/ \ 
 -a filename \
 -p /home/leonard/Applications/plantcv/tray_vis.py \
